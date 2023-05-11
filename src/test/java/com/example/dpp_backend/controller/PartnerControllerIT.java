e4ffe27ba9a0add80117a8fdb1957a94d17ede75
Leonardo Almeida <leonardoalmeida7@ua.pt>
2023-05-11 09:37:03 +0100

    remove code smell

--- src/test/java/com/example/dpp_backend/controller/PartnerControllerIT.java
+++ src/test/java/com/example/dpp_backend/controller/PartnerControllerIT.java
@@ -20,7 +20,7 @@ import static org.hamcrest.Matchers.*;
 
 // integration tests with RestAssured and hamcrest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PartnerControllerIT {
+class PartnerControllerIT {
 
     @LocalServerPort
     private int port;
