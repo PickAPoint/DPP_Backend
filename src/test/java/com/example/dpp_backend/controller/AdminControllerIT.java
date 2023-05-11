d5deb2b5fa4dc14b332e3bc5f53881a6fc949d5c
Leonardo Almeida <leonardoalmeida7@ua.pt>
2023-05-11 21:01:39 +0100

    PIC-61 validate users

--- src/test/java/com/example/dpp_backend/controller/AdminControllerIT.java
+++ src/test/java/com/example/dpp_backend/controller/AdminControllerIT.java
@@ -3,6 +3,7 @@ package com.example.dpp_backend.controller;
 import com.example.dpp_backend.model.User;
 import com.example.dpp_backend.repository.UserRepository;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,41 +30,42 @@ class AdminControllerIT {
 
         RestAssured.port = port;
 
-        userRepository.flush();
-
         user1 = new User();
         user1.setId(1);
         user1.setEmail("user1@test.com");
         user1.setPassword("test");
         user1.setType("Pending");
-
-        userRepository.save(user1);
     }
+
 
     @DisplayName("Get all users")
     @Test
     void testGetAllUsers() {
 
-            RestAssured.given()
-                    .contentType("application/json")
-                    .when()
-                    .get("/admin/users")
-                    .then()
-                    .statusCode(200)
-                    .body("$", hasSize(1))
-                    .body("[0].email", equalTo("user1@test.com"));
+        userRepository.saveAndFlush(user1);
+
+        RestAssured.given()
+                .contentType("application/json")
+                .when()
+                .get("/admin/users")
+                .then()
+                .statusCode(200)
+                .body("$", hasSize(1))
+                .body("[0].email", equalTo("user1@test.com"));
     }
 
     @DisplayName("Validate user (valid)")
     @Test
     void testValidateUserValid() {
 
-            RestAssured.given()
-                    .contentType("application/json")
-                    .when()
-                    .post("/admin/validate/1")
-                    .then()
-                    .statusCode(200)
-                    .body(equalTo("true"));
+        userRepository.saveAndFlush(user1);
+
+        RestAssured.given()
+                .contentType("application/json")
+                .when()
+                .post("/admin/validate/1")
+                .then()
+                .statusCode(200)
+                .body(equalTo("true"));
     }
 }
